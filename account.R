8d9b992a8e64c176245461dd37352252d7cddef0
wy123123 <wangyu0917@hotmail.com>
2015-03-20 13:42:12 +0800

    code

---
+++ account.R
@@ -0,0 +1,97 @@
+library(xlsx)
+library(data.table)
+####################################################
+month=c("Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec")
+for(i in 1 : 12){
+        url=paste("C:/Users/Lovebonito/Desktop/d/",i,".csv",sep = "")
+        #url=paste("C:/Users/Lovebonito/Desktop/d/12.csv")
+        dt=read.csv(url)
+        if(i==1){
+                dt.all=dt
+        }
+        dt=dt[-c(length(dt[,1]),length(dt[,1])-1),]
+        dt.all=rbind(dt.all,dt)
+        s=strsplit(as.character(dt$Transaction.Date),split=" ")
+        m=matrix(unlist(s),ncol=2,byrow=T)
+        dates <- strftime(m[,1],"%Y-%m-%d",usetz = FALSE)
+        
+        dt.12=data.table(cbind(dt,dates))
+        
+        table(dt.12$Bank)
+        
+        dt.a=dt.12[dt.12$Bank!="AMEX",]
+        a=dt.a[,sum(Amount),by=list(dates)]
+        
+        dt.b=dt.12[dt.12$Bank == "AMEX",]
+        b=dt.b[,sum(Amount),by=list(dates)]
+        
+        a$AMEX=b$V1[match(a$dates,b$dates)]
+        
+        all=data.frame(Dates=a$dates,ENETS=a$V1,AMEX=a$AMEX)
+        setnames(all,c("Dates","ENETS","AMEX"))
+        
+        c=data.frame(Dates=c("sum_all",sum(all$ENETS,all$AMEX,na.rm=T)),ENETS=c("sum_enets",sum(all$ENETS)),AMEX=c("sum_amex",sum(all$AMEX,na.rm=T)))
+        if(sum(is.na(all$AMEX))!= 0){
+                all[is.na(all$AMEX),]$AMEX = 0
+        }
+        
+        f=rbind(all,c)
+        write.xlsx(f,"C:/Users/Lovebonito/Desktop/final.xlsx",sheetName=month[i],append=T)
+}
+
+########
+#AMEX
+########
+for( i in 1:12){
+        dt=read.xlsx("C:/Users/Lovebonito/Desktop/d/final.xlsx",sheetName=month[i])
+        dt.AMEX=data.frame(Dates=dt$Dates,AMEX=dt$AMEX)
+        write.xlsx(dt.AMEX,"C:/Users/Lovebonito/Desktop/d/AMEX.xlsx",sheetName=month[i],append=T,row.names=F)
+}
+########
+##Jan
+########
+Jan=(read.table("C:/Users/Lovebonito/Desktop/d/AMEX/AMEX Jan.txt",header=FALSE,sep=" "))
+Jan.1=0
+for(i in 1 :198){
+        Jan.1[i]=as.vector(Jan[1,i])
+}
+df=data.frame(matrix(Jan.1,ncol=6,byrow=T))
+setnames(df,c("Submission.Dates","Summary","Gross.AMT","Net.AMT","Settlement.AMT","Settlement.Date"))
+write.xlsx(df,"C:/Users/Lovebonito/Desktop/d/AMEX/jan.xlsx",sheetName="1",row.names=F)
+
+df.2=read.xlsx("C:/Users/Lovebonito/Desktop/d/AMEX/2paymentstatement-Feb14.xlsx",sheetIndex=1,startRow=3,header=F, stringsAsFactors=F)
+df.f=data.frame(Submission.date=df.2$X5,Settlment.date=df.2$X6,Gross.AMT=df.2$X9,Discount.AMT=df.2$X12,Net.AMT=df.2$X15,ds=df.2$X17)
+df.f=df.f[!is.na(df.f$Submission.date),]
+write.xlsx(df.f,"C:/Users/Lovebonito/Desktop/d/AMEX/feb.xlsx",sheetName="1",row.names=F)
+
+df.2=read.xlsx("C:/Users/Lovebonito/Desktop/d/AMEX/12paymentstatement-Dec14.xlsx",sheetIndex=1,startRow=4,header=F, stringsAsFactors=F)
+df.f=data.frame(Submission.date=df.2$X5,Settlment.date=df.2$X6,Gross.AMT=df.2$X9,Discount.AMT=df.2$X12,Net.AMT=df.2$X15,ds=df.2$X17)
+df.f=df.f[!is.na(df.f$Submission.date),]
+df.f$Submission.date=as.Date(as.character(df.f$Submission.date),origin = "1899-12-30")
+write.xlsx(df.f,"C:/Users/Lovebonito/Desktop/d/AMEX/Dec.xlsx",sheetName="1",row.names=F)
+
+
+Janx=read.xlsx("C:/Users/Lovebonito/Desktop/d/AMEX.xlsx",sheetName="Jan")
+Janx.1=Janx[!is.na(Janx$AMEX),]
+Janx.1=Janx.1[as.numeric(as.character(Janx.1$AMEX))!=0,]
+Janx.1=Janx.1[!is.na(Janx.1$AMEX),]
+Janx.2=Janx.1[substring(as.character(Janx.1$Dates),1,1)=="2",]
+
+df$Submission.Dates=strptime(df$Submission.Dates,"%d/%m/%y")
+df$Settlement.Date=strptime(df$Settlement.Date,"%d/%m/%y")
+Janx.2$Dates=strptime(Janx.2$Dates,"%Y-%m-%d")
+Janx.2$Dates[29] == df$Submission.Dates[4]
+df.1=df[!duplicated(df[,c(5,6)]),c(5,6)]
+
+df$Dates=Janx.2$Dates[match(df$Submission.Dates,Janx.2$Dates)]
+df$AMT=Janx.2$AMEX[match(df$Submission.Dates,Janx.2$Dates)]
+df$Date.S=df.1$Settlement.AMT[match(df$Submission.Dates,df.1$Settlement.Date)]
+
+table(match(Janx.2$Dates,df$Submission.Dates))
+Janx.2$Dates[Janx.2$Dates %in% df$Submission.Dates]
+################################################
+#read bank statement
+###############################################
+
+dir="C:/Users/Lovebonito/Desktop/wan yin finance/statement"
+month=c("Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec")
---
+++ machinel.R
@@ -0,0 +1,81 @@
+library(caret)
+library(MASS)
+
+url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
+file <- file.path(getwd(),"m.csv")
+download.file(url,file)
+dt.train <- read.csv(file)
+
+url <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+file <- file.path(getwd(),"t.csv")
+download.file(url,file)
+dt.test <- read.csv(file)
+
+
+##################
+#feature selection
+##################
+dt.train.1 <- subset(dt.train,select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window))
+dt.test.1 <-subset(dt.test,select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window)) 
+#remove NA columns
+dt.train.2=dt.train.1[,colSums(is.na(dt.1))<19622*0.1]
+dt.test.2=dt.test.1[,colSums(is.na(dt.1))<20*0.1]
+#near near 0 variance columns
+dt.train.3 <- nearZeroVar(dt.train.2,saveMetrics=T)
+dt.train.4 <- dt.train.2[,!dt.train.3$nzv]
+dt.test.3 <- nearZeroVar(dt.test.2,saveMetrics=T)
+dt.test.4 <- dt.test.2[,!dt.test.3$nzv]
+#remove the response variable
+dt.train.4.v <- subset(dt.train.4,select=-classe)
+dt.test.4.v <- subset(dt.test.4,select=-classe)
+#PCA for further dimention reduction
+pre <- preProcess(dt.train.4.v,method="pca")
+#tansforme data
+trainP=predict(pre,dt.train.4.v)
+testP=predict(pre,dt.test.4.v)
+
+#model selection
+mod.1 <- train(dt.train.4$classe~.,method="rf",data=trainP)
+mod.2 <- train(dt.train.4$classe~.,method="gbm",data=trainP)
+mod.3 <- train(dt.train.4$classe~.,method="rpart",data=trainP)
+mod.4 <- train(dt.train.4$classe~.,method="lda",data=trainP)
+
+confusionMatrix(dt.train.4$classe,predict(mod.4,trainP))
+varImp(mod.1)
+
+#random forest is the most accurate model.
+
+##################
+#Cross validation, k-fold 
+##################
+set.seed(123)
+#create 10 folds, find the average sample error of the 10 sets of data
+folds.train <- createFolds(y=dt.train$classe,k=10,list=T,returnTrain=T)
+folds.test<- createFolds(y=dt.train$classe,k=10,list=T,returnTrain=F)
+cmatrix=list()
+for(i in 1 : 10){
+        
+        dt.train.1 <- subset(dt.train[folds.train[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window))
+        dt.test.1 <-subset(dt.train[folds.test[[i]],],select=-c(X,user_name,raw_timestamp_part_1,raw_timestamp_part_2,cvtd_timestamp,num_window)) 
+        #remove NA columns
+        dt.train.2=dt.train.1[,colSums(is.na(dt.1))<length(folds.train[[i]])*0.1]
+        dt.test.2=dt.test.1[,colSums(is.na(dt.1))<length(folds.test[[i]])*0.1]
+        #near near 0 variance columns
+        dt.train.3 <- nearZeroVar(dt.train.2,saveMetrics=T)
+        dt.train.4 <- dt.train.2[,!dt.train.3$nzv]
+        dt.test.3 <- nearZeroVar(dt.test.2,saveMetrics=T)
+        dt.test.4 <- dt.test.2[,!dt.test.3$nzv]
+        #remove the response variable
+        dt.train.4.v <- subset(dt.train.4,select=-classe)
+        dt.test.4.v <- subset(dt.test.4,select=-classe)
+        #PCA for further dimention reduction
+        pre <- preProcess(dt.train.4.v,method="pca")
+        #tansforme data
+        trainP <- predict(pre,dt.train.4.v)
+        testP <- predict(pre,dt.test.4.v)
+        
+        mod <- train(dt.train.4$classe~.,method="rf",data=trainP)
+        cmatrix[[i]] <- confusionMatrix(dt.test.4$classe,predict(mod,testP))$overall
+}
+
+mean(as.numeric(unlist(lapply(cmatrix,'[[',1))))
